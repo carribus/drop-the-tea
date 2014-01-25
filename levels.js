80d41df87f200859b344a7f1a8da3f862cccbeb4
alexbiet <alexandrubiet@gmail.com>
2014-01-25 14:07:51 +0100

    ...

--- levels.js
+++ levels.js
@@ -6,10 +6,17 @@ var levels = [{
             position: {x: 10, y: 10}
       },
       {
-            type: 'b',
-            velocity: {x: 10, y: 0},
+            type: 'b', // maze
+            velocity: {x: 0, y: 0},
             flip: 100,
             position: {x: 12, y: 12},
-            size: {w: 1, h:1}
+            size: {w: 5, h:1}
+      },
+      {
+            type: 'b', // cup
+            velocity: {x: 0, y: 0},
+            flip: 120,
+            position: {x: 26, y: 25.8},
+            size: {w: 4, h:4}
       }]
 }];
