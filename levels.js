cde80f4a32717d1d6a29b7c98b205655b4832e5a
Daniel Cohen <daniel.cohen@odobo.com>
2014-01-25 13:52:51 +0100

    squares in levels

---
+++ levels.js
@@ -0,0 +1,15 @@
+var levels = [{
+      shapes: [{
+            type: 'c',
+            velocity: {x: 0, y: 0},
+            flip: 100,
+            position: {x: 10, y: 10}
+      },
+      {
+            type: 'b',
+            velocity: {x: 10, y: 0},
+            flip: 100,
+            position: {x: 12, y: 12},
+            size: {w: 1, h:1}
+      }]
+}];
