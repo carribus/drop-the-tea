60032053ff9fda52de9be5ae7f7584b32d9b80f3
Peter Mares <peter@odobo.com>
2014-01-25 13:15:54 +0100

    Merged collision detector.

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ var b2Vec2 = Box2D.Common.Math.b2Vec2,
       b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape,
       b2CircleShape = Box2D.Collision.Shapes.b2CircleShape,
       b2DebugDraw = Box2D.Dynamics.b2DebugDraw,
-      b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef;
+      b2MouseJointDef = Box2D.Dynamics.Joints.b2MouseJointDef,
+      b2ContactListener = Box2D.Dynamics.b2ContactListener;
 
 
 var world = new b2World(
@@ -62,147 +63,11 @@ function addBag() {
 }
 
 function init() {
-
-    // create the world's contact listener
-    world.SetContactListener(contactListener);
-
-            //create ground
-            bodyDef.type = b2Body.b2_staticBody;
-            //Top bottom limits
-            fixDef.shape = new b2PolygonShape;
-            fixDef.shape.SetAsBox(40, 2);
-            bodyDef.position.Set(10, 900 / 30 + 1.8);
-            world.CreateBody(bodyDef).CreateFixture(fixDef);
-            bodyDef.position.Set(10, -1.8);
-            world.CreateBody(bodyDef).CreateFixture(fixDef);
-
-            //Left Right
-            fixDef.shape.SetAsBox(2, 18);
-            bodyDef.position.Set(-1.8, 17);
-            world.CreateBody(bodyDef).CreateFixture(fixDef);
-            bodyDef.position.Set(1400 / 30 + 1.8, 13);
-            world.CreateBody(bodyDef).CreateFixture(fixDef);
-
-	//create the peg
-	peg = new b2BodyDef;
-	peg.type = b2Body.b2_staticBody;
-	fixDef.shape = new b2CircleShape(0.1);
-	peg.position.x = 10;
-	peg.position.y = 1;
-	peg = world.CreateBody(peg).CreateFixture(fixDef);
-
-	//create the bag attached to the peg
-
-
-	//create some objects
-	// bodyDef.type = b2Body.b2_dynamicBody;
-	// for (var i = 0; i < 10; ++i) {
-	// 	if (Math.random() > 0.5) {
-	// 		fixDef.shape = new b2PolygonShape;
-	// 		fixDef.shape.SetAsBox(
-	// 			Math.random() + 0.1 //half width
-	// 			, Math.random() + 0.1 //half height
-	// 		);
-	// 	} else {
-	// 		fixDef.shape = new b2CircleShape(
-	// 			Math.random() + 0.1 //radius
-	// 		);
-	// 	}
-	// 	bodyDef.position.x = Math.random() * 10;
-	// 	bodyDef.position.y = Math.random() * 10;
-	// 	world.CreateBody(bodyDef).CreateFixture(fixDef);
-	// }
-
-	//setup debug draw
-	var debugDraw = new b2DebugDraw();
-	debugDraw.SetSprite(document.getElementById("canvas").getContext("2d"));
-	debugDraw.SetDrawScale(30.0);
-	debugDraw.SetFillAlpha(0.5);
-	debugDraw.SetLineThickness(1.0);
-	debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
-	world.SetDebugDraw(debugDraw);
-
-	window.setInterval(update, 1000 / 60);
-
-	//mouse
-
-	var mouseX, mouseY, mousePVec, isMouseDown, selectedBody, mouseJoint;
-	var canvasPosition = getElementPosition(document.getElementById("canvas"));
-
-	document.addEventListener("mousedown", function(e) {
-		mouseX = (e.clientX - canvasPosition.x) / 30;
-		mouseY = (e.clientY - canvasPosition.y) / 30;
-		var clickedBag = getBodyAtMouse();
-		if (clickedBag) {
-			world.DestroyJoint(clickedBag.string);
-		}
-	}, true);
-
-
-	function getBodyAtMouse() {
-		mousePVec = new b2Vec2(mouseX, mouseY);
-		var aabb = new b2AABB();
-		aabb.lowerBound.Set(mouseX - 0.001, mouseY - 0.001);
-		aabb.upperBound.Set(mouseX + 0.001, mouseY + 0.001);
-
-		// Query the world for overlapping shapes.
-		selectedBody = null;
-		world.QueryAABB(getBodyCB, aabb);
-		return selectedBody;
-	}
-
-	function getBodyCB(fixture) {
-		if (fixture.GetBody().GetType() != b2Body.b2_staticBody) {
-			if (fixture.GetShape().TestPoint(fixture.GetBody().GetTransform(), mousePVec)) {
-				selectedBody = fixture.GetBody();
-				return false;
-			}
-		}
-		return true;
-	}
-
-	//update
-	function update() {
-
-		world.Step(1 / 60, 10, 10);
-		world.DrawDebugData();
-		world.ClearForces();
-	};
-
-	//helpers
-
-	//http://js-tut.aardon.de/js-tut/tutorial/position.html
-	function getElementPosition(element) {
-		var elem = element,
-			tagname = "",
-			x = 0,
-			y = 0;
-
-		while ((typeof(elem) == "object") && (typeof(elem.tagName) != "undefined")) {
-			y += elem.offsetTop;
-			x += elem.offsetLeft;
-			tagname = elem.tagName.toUpperCase();
-
-			if (tagname == "BODY")
-				elem = 0;
-
-			if (typeof(elem) == "object") {
-				if (typeof(elem.offsetParent) == "object")
-					elem = elem.offsetParent;
-			}
-		}
-
-		return {
-			x: x,
-			y: y
-		};
-	}
-
-
-};
-
-function init() {
       var map;
+
+      // create the world's contact listener
+      world.SetContactListener(contactListener);
+
       bodyDef.type = b2Body.b2_kinematicBody;
       fixDef.shape = new b2CircleShape(0.4);
       for (var i = 1; i < 10; ++i) {
@@ -254,6 +119,7 @@ function init() {
 
 
       //create some objects
+      addTeaCup()
 
 
       //setup debug draw
